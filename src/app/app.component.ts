1b268373ea898edbddb69f18b8bfb3797d4aa1c3
MarkAWulf <mark.wulf@gmail.com>
2025-05-16 23:06:12 -0500

    in the beginning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { CardComponent } from "./card/card.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ CommonModule, CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    { title: 'Neat Tree',
+      imageUrl: 'tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat tree today'
+    },
+    { title: 'Snowy Mountain',
+      imageUrl: 'mountain.jpeg',
+      username: 'Moutainlover',
+      content: 'here is a pic of a snowy mountain'
+    },
+    { title: 'Mountain Bike',
+      imageUrl: 'biking.jpeg',
+      username: 'biking12222',
+      content: 'i did some biking today'
+    }
+  ];
 }
